3439530260ec2213c8a81259ebbd192411c34b0b
leedanbii <eksqlgodqhr@naver.com>
2025-11-03 03:34:48 +0900

    refactor(view): 사용자 가독성을 위해 InputView 입력 문자열 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,9 +3,9 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String READ_AMOUNT_MESSAGE = "구입 금액을 입력해 주세요.";
-    private static final String READ_WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String READ_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String READ_AMOUNT_MESSAGE = "\n구입금액을 입력해 주세요.";
+    private static final String READ_WINNING_NUMBERS_MESSAGE = "\n당첨 번호를 입력해 주세요.";
+    private static final String READ_BONUS_NUMBER_MESSAGE = "\n보너스 번호를 입력해 주세요.";
 
     public String readAmount() {
         System.out.println(READ_AMOUNT_MESSAGE);
